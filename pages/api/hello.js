f5f40dc22149eae615873609dd46ae773aacb466
Julius-R <julius.robinson95@yahoo.com>
2021-08-28 22:49:31 -0600

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getAllPostsWithSlug } from "@/lib/api";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+	const response = await getAllPostsWithSlug();
+	console.log(response);
+	res.status(200).json(response);
 }
